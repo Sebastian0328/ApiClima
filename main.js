45ed7971f74f63d0a1812464bf98e4dd85705e01
sebastian alvarez <alvarezpaezsebastian03@gmail.com>
2023-06-15 08:47:26 +0200

    terminado

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const city = document.querySelector("#city")
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     if (city.value === '') {
-        result.innerHTML = `<p>No hay ningun valor escrito</p>
-                                <p>Por favor escriba su ciudad</p>`;
+        result.innerHTML = `<p id="alert">No hay ningun valor escrito</p>
+                                <p id= "alert">Por favor escriba su ciudad</p>`;
         return
 
     }
@@ -17,8 +17,25 @@ async function calApi(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0b9d58e1e7148fe386bd34a5098fda83`);
     const data = await response.json()
     console.log(data);
-    result.innerHTML = `<h2>clima actual</h2>
-    <p>${data.name}</p>
-    <img src="" alt="">
-    <p>velocidad del veinto</p>`
+    const clima = data.main.temp
+    const kelvin = kelvinToC(clima)
+    const icon = data.weather[0].icon
+    const viento = data.wind.speed
+    const speed = kilometroporhora(viento)
+    console.log(viento);
+
+
+    function kelvinToC(clima) {
+        return parseInt(clima - 273.15)
+    }
+    kelvinToC(clima)
+    function kilometroporhora(viento) {
+        return parseInt(viento * 3.6)
+
+    }
+    kilometroporhora()
+    result.innerHTML = `<p class="name">${data.name}</p>
+    <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="">
+    <h2>${kelvin}°C</h2>
+    <p>velocidad del veinto:${speed}</p>`
 }
